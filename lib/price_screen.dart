08d4c8ceb38dfa6d8c5595a21469c1d743997043
Okashe <okashabdi88@gmail.com>
2023-06-13 19:33:33 -0500

    project complete

--- lib/price_screen.dart
+++ lib/price_screen.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter/cupertino.dart';
 import 'coin_data.dart';
 import 'dart:io' show Platform; //to see which platfrom our code is run on.
+import 'result.dart';
 
 class PriceScreen extends StatefulWidget {
   const PriceScreen({super.key});
@@ -150,46 +151,6 @@ class _PriceScreenState extends State<PriceScreen> {
             child: Platform.isIOS ? iOSPicker() : androidDropdown(),
           ),
         ],
-      ),
-    );
-  }
-}
-
-//1: Refactor this Padding Widget into a separate Stateless Widget called CryptoCard, so we can create 3 of them, one for each cryptocurrency.
-class CryptoCard extends StatelessWidget {
-  //2: You'll need to able to pass the selectedCurrency, value and cryptoCurrency to the constructor of this CryptoCard Widget.
-  const CryptoCard({
-    super.key,
-    this.value,
-    this.selectedCurrency,
-    this.cryptoCurrency,
-  });
-
-  final String? value;
-  final String? selectedCurrency;
-  final String? cryptoCurrency;
-
-  @override
-  Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.fromLTRB(18.0, 18.0, 18.0, 0),
-      child: Card(
-        color: Colors.lightBlueAccent,
-        elevation: 5.0,
-        shape: RoundedRectangleBorder(
-          borderRadius: BorderRadius.circular(10.0),
-        ),
-        child: Padding(
-          padding: const EdgeInsets.symmetric(vertical: 15.0, horizontal: 28.0),
-          child: Text(
-            '1 $cryptoCurrency = $value $selectedCurrency',
-            textAlign: TextAlign.center,
-            style: const TextStyle(
-              fontSize: 20.0,
-              color: Colors.white,
-            ),
-          ),
-        ),
       ),
     );
   }
--- lib/result.dart
+++ lib/result.dart
@@ -1,6 +1,19 @@
 import 'package:flutter/material.dart';
 
-class Result extends StatelessWidget {
+//1: Refactor this Padding Widget into a separate Stateless Widget called CryptoCard, so we can create 3 of them, one for each cryptocurrency.
+class CryptoCard extends StatelessWidget {
+  //2: You'll need to able to pass the selectedCurrency, value and cryptoCurrency to the constructor of this CryptoCard Widget.
+  const CryptoCard({
+    super.key,
+    this.value,
+    this.selectedCurrency,
+    this.cryptoCurrency,
+  });
+
+  final String? value;
+  final String? selectedCurrency;
+  final String? cryptoCurrency;
+
   @override
   Widget build(BuildContext context) {
     return Padding(
@@ -11,12 +24,12 @@ class Result extends StatelessWidget {
         shape: RoundedRectangleBorder(
           borderRadius: BorderRadius.circular(10.0),
         ),
-        child: const Padding(
-          padding: EdgeInsets.symmetric(vertical: 15.0, horizontal: 28.0),
+        child: Padding(
+          padding: const EdgeInsets.symmetric(vertical: 15.0, horizontal: 28.0),
           child: Text(
-            '1 BTC =  USD',
+            '1 $cryptoCurrency = $value $selectedCurrency',
             textAlign: TextAlign.center,
-            style: TextStyle(
+            style: const TextStyle(
               fontSize: 20.0,
               color: Colors.white,
             ),
